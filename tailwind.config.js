a73cd41d0cb517fd7fbcbd3256883311136c200e
ducsakun <vanducvt0305@gmail.com>
2024-11-20 15:24:16 +0700

    add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      borderRadius: {
+        "5xl": "5rem",
+      },
+    },
   },
   plugins: [],
 };
